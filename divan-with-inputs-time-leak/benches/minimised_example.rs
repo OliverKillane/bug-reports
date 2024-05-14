262a844c7c1bd9c02866b08dfcac6650a346f405
Oliver Killane <oliverkillane@gmail.com>
2024-05-15 00:46:44 +0100

    Fixed non-bug (mistaken use of with_values instead of with_refs

--- divan-with-inputs-time-leak/benches/minimised_example.rs
+++ divan-with-inputs-time-leak/benches/minimised_example.rs
@@ -1,5 +1,4 @@
-//! A minimised example for with_inputs affecting time
-//! - Each benchmark should have identical time
+//! Benchmark should have no time
 
 #[divan::bench(
     consts = [100,1000,10000,100000],
@@ -11,46 +10,15 @@ fn with_sleep<const SLEEP_TIME: u64>(bencher: divan::Bencher) {
             use std::{thread, time::Duration};
             thread::sleep(Duration::from_micros(SLEEP_TIME))
         })
-        .bench_values(|_| () ) 
-}
-
-
-#[divan::bench(
-    consts = [1,10,100,1000],
-)]
-fn with_iterator<const SIZE: usize>(bencher: divan::Bencher) {
-    bencher
-        .with_inputs(|| {
-            (0..SIZE).collect::<Vec<_>>()
-        })
-        .bench_values(|_| () ) 
+        .bench_refs(|_| ())
 }
 
 #[divan::bench(
-    consts = [1,10,100,1000],
+    consts = [100,1000,10000,100000],
+    max_time = 0.1
 )]
-fn with_loop<const SIZE: usize>(bencher: divan::Bencher) {
-    bencher
-        .with_inputs(|| {
-            let mut a = Vec::with_capacity(SIZE);
-            for i in 0..SIZE {
-                a.push(i);
-            }
-            a
-        })
-        .bench_values(|_| () ) 
-}
-
-
-#[divan::bench(
-    consts = [1,10,100,1000],
-)]
-fn with_nothing<const SIZE: usize>(bencher: divan::Bencher) {
-    bencher
-        .with_inputs(|| {
-            ()
-        })
-        .bench_values(|_| () ) 
+fn with_nothing<const SLEEP_TIME: u64>(bencher: divan::Bencher) {
+    bencher.with_inputs(|| ()).bench_refs(|_| ())
 }
 
 fn main() {
